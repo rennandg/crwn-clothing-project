84614e4f6e3d13eb4d4cb2566ddef2083daf5bcb
unknown <rennand1988@gmail.com>
2021-02-01 16:02:14 +0800

    broken homepage into smaller component and added styling menu item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './homepage.component'
+import Homepage from './pages/homepage/homepage.component.jsx'
 
 function App() {
   return <div><Homepage /></div>;

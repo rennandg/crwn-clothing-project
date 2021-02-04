eb46995d44c3d1a0296f1e8bd52a84ed9cb44bd3
unknown <rennand1988@gmail.com>
2021-02-04 17:22:15 +0800

    update our up to redirect when user sign in and also useMapDispatch useStateProps and connect to change header state

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -48,7 +48,7 @@ class SignIn extends React.Component {
                     <FormInput name='password' type='password' handleChange={this.handleChange} value={this.state.password} label='password' required />
                  <div className='buttons'>  
                     <CustomButton type='submit'>SIGN IN</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn > Sign in with Google</CustomButton>
+                    <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn > Sign in with Google</CustomButton>
                  </div>   
                 </form>
             </div>

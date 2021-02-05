c888b985918b392a5a91b5d43e54a68fd963862c
unknown <rennand1988@gmail.com>
2021-02-05 12:43:45 +0800

    created cart item component and connected cart dropdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,14 +1,17 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id
-        );
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
 
-    if (existingCartItem) {
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem 
-        )
-    }
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
 
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }] //quantity property get attached the first time around since this if block won't run when it's a new item!
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+//quantity property get attached the first time around since this if block won't run when it's a new item!
